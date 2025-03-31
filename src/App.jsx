edac2d4e3a9eeec3af9744c46fc79939a0eb5ad5
Fideliog <eduardoledo36@gmail.com>
2025-03-30 21:40:22 -0300

    Primeiro usando bash

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ListaTarefas from "./componentes/ListaTarefas";
+
+import './App.css';
+
+function App(){
+
+  return (
+    <>
+      <h1>Gerenciador de Tarefas</h1>
+      <ListaTarefas />
+    </>
+  );
+}
+
+export default App;
